9758f1b145163024eb3c37272367228844e61d53
seanm <sean.milburn@aptosolutions.co.uk>
2016-03-06 23:03:18 +0000

    Added service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,39 +19,18 @@ angular
         redirectTo: '/main'
       });
   })
-  .controller('FlickrController', function($scope, $rootScope, $window) {
-    $scope.fetchPhotos = function(){
-      $scope.array = [];
-      $scope.failed = false;        
-      $scope.isFetching = true;
-      $.ajax({
-        url: "https://api.flickr.com/services/feeds/photos_public.gne?tags=" + $scope.query + "&tagmode=all&format=json",
-        dataType: "jsonp",
-        type: 'GET',
-        jsonpCallback: 'jsonFlickrFeed',            
-        success: function(feeds){
-          $scope.$apply(function(){
-            $rootScope.feeds = $scope.array.concat(feeds.items);
-            $scope.isFetching = false;
-            $scope.failed = false;
-            $scope.limit= 5;
-            $scope.loadMore = function() {
-              $scope.limit = $scope.limit + 5;
-              console.log($scope.limit)
-            }
-          });
-      },
-      error: function(error){
-          $scope.$apply(function(){
-            $scope.failed = true;                                   
-            $scope.isFetching = false;    
-          });
-        }
-      });
-    };
-    $scope.$on('$viewContentLoaded', function() {
-        $window.scrollTo(0, 0);
-    });
+  .factory('FlickrService', function ($resource) {
+    return $resource('http://api.flickr.com/services/feeds/photos_public.gne', { format: 'json', jsoncallback: 'JSON_CALLBACK' }, { 'load': { 'method': 'JSONP' } });
+  })
+  .controller('FlickrController', function($scope, $rootScope, $window, FlickrService) {    
+    $scope.limit = 5;
+    $scope.fetchPhotos = function() {
+      $rootScope.feeds = FlickrService.load({ tags: $scope.query });
+    }
+    $scope.loadMore = function() {
+      $scope.limit = $scope.limit + 5;
+      console.log($scope.limit)
+    }
     $('#search-bar').keypress(function(e){
         if(e.which == 13){//Enter key pressed
             $('#search-button').click();//Trigger search button click event
@@ -59,13 +38,12 @@ angular
     });
     $scope.fetchPhotos();
   })
-  .controller('PhotoController', function($scope, $rootScope, $window) {
+  .controller('PhotoController', function($scope, $rootScope, $window, FlickrService) {
     $scope.choosePhoto = function() {
       $scope.photoIndex = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
-      $scope.chosenPhoto = $rootScope.feeds[$scope.photoIndex];
+      $scope.chosenPhoto = $scope.feeds.items[$scope.photoIndex];
       $scope.photoTags = $scope.chosenPhoto.tags;
     }
-    $scope.choosePhoto();
   });
   
 
