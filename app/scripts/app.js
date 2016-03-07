01e2f77961d7dfacb0e59aca874519c822565f0d
Sean Milburn <sean.milburn@digital.hmrc.gov.uk>
2016-03-07 13:10:36 +0000

    Updated bower.json and added comments to code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,44 +5,56 @@ angular
     'ngResource',
     'ngRoute'
   ])
+  //Provides page routes and adds controllers accordingly
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/main', {
+      .when('/main', {//main page
         templateUrl: 'views/main.html',
         controller: 'FlickrController'
       })
-      .when('/photo/:id*', {
+      .when('/photo/:id*', {//detail page
         templateUrl: 'views/photo.html',
         controller: 'PhotoController'
       })
       .otherwise({
-        redirectTo: '/main'
+        redirectTo: '/main'//fall back
       });
   })
+  //Factory service for fetching data from the API
   .factory('FlickrService', function ($resource) {
-    return $resource('http://api.flickr.com/services/feeds/photos_public.gne', { format: 'json', jsoncallback: 'JSON_CALLBACK' }, { 'load': { 'method': 'JSONP' } });
+    return $resource('http://api.flickr.com/services/feeds/photos_public.gne',
+      { format: 'json', jsoncallback: 'JSON_CALLBACK' }, 
+      { 'load': { 'method': 'JSONP' } 
+    });
   })
+  //Controller for main page
   .controller('FlickrController', function($scope, $rootScope, $window, FlickrService) {    
-    $scope.limit = 5;
+    $scope.limit = 5; //Limits number of photos to 5
+    //Function for fetching photographs
     $scope.fetchPhotos = function() {
-      $rootScope.feeds = FlickrService.load({ tags: $scope.query });
+      $rootScope.feeds = FlickrService.load({ tags: $scope.query });//Adds custom search query to url
     }
+    //Function for loading more images
     $scope.loadMore = function() {
-      $scope.limit = $scope.limit + 5;
-      console.log($scope.limit)
+      if ($scope.limit !== 20) {//If the limit is not 20
+        $scope.limit += 5;//Add another 5
+      } else {
+        $('#load-more').hide();//else hide button
+      }
     }
     $('#search-bar').keypress(function(e){
         if(e.which == 13){//Enter key pressed
             $('#search-button').click();//Trigger search button click event
         }
     });
-    $scope.fetchPhotos();
+    $scope.fetchPhotos();//Call fetchPhoto function
   })
+  //Controller for photo detail page
   .controller('PhotoController', function($scope, $rootScope, $window, FlickrService) {
     $scope.choosePhoto = function() {
-      $scope.photoIndex = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
-      $scope.chosenPhoto = $scope.feeds.items[$scope.photoIndex];
-      $scope.photoTags = $scope.chosenPhoto.tags;
+      $scope.photoIndex = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)//Takes the index of the photo from the url
+      $scope.chosenPhoto = $scope.feeds.items[$scope.photoIndex];//Creates scope for chosen photo using the index
+      $scope.photoTags = $scope.chosenPhoto.tags;//Creates scope for the tags
     }
   });
   
