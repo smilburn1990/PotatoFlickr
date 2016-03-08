18e0dcdded5c75803a5d87cf6b0c15572853b45e
Sean Milburn <sean.milburn@digital.hmrc.gov.uk>
2016-03-08 16:21:14 +0000

    Configuration of Karma unit tests

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -1,23 +1,19 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: PhotoController', function () {
 
   // load the controller's module
   beforeEach(module('potatoFlickrApp'));
 
-  var AboutCtrl,
+  var PhotoController,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    PhotoController = $controller('PhotoController', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(AboutCtrl.awesomeThings.length).toBe(3);
-  });
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,23 +1,19 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: FlickrController', function () {
 
   // load the controller's module
   beforeEach(module('potatoFlickrApp'));
 
-  var MainCtrl,
+  var FlickrController,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    FlickrController = $controller('FlickrController', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
-  });
 });
